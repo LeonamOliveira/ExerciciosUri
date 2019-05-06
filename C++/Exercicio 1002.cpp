ab9000fe8fd840aa5d75dd896d4575bbe38571eb
Leonam Oliveira <leonampucgo@gmail.com>
2019-05-05 23:05:03 -0300

    Adicionando exercícios 1002 e 1003.

---
+++ C++/Exercicio 1002.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double area, raio, n = 3.14159;
+    cin >> raio;
+    area = n * (raio* raio);
+    cout << fixed << setprecision(4) << "A" << "=" << area << endl ;
+}
+
---
+++ C++/Exercicio 1003.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,soma;
+    cin >> a >> b;
+    soma = a + b;
+    cout << "SOMA = " << soma << endl;
+    return 0;
+}
